8c2cbc4d4abe020e045eaedab8974fac7c5a2a26
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-06-05 19:09:47 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/modeloDominioORM/ModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modeloDominioORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/modeloDominioORM/ModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modeloDominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
